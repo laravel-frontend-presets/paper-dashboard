c76c8e4bc81432cd4f5a64a115cc71bca3ff9044
creativetimofficial <team@updivision.com>
2022-05-30 10:57:21 +0000

    Update from https://github.com/laravel-frontend-presets/paper-dashboard/commit/08b8fb5b67303a5d5d42cf3a644e90206d820e11

--- src/paper-dashboard-stubs/resources/views/layouts/page_templates/guest.blade.php
+++
@@ -1,8 +0,0 @@
-@include('layouts.navbars.navs.guest')
-
-<div class="wrapper wrapper-full-page ">
-    <div class="full-page section-image" filter-color="black" data-image="{{ asset('paper') . '/' . ($backgroundImagePath ?? "img/bg/fabio-mangione.jpg") }}">
-        @yield('content')
-        @include('layouts.footer')
-    </div>
-</div>
